5b97faa220aa6b827b3bc36261ab734714c215ae
kprudnikov <kprudnikov@gmail.com>
2015-05-16 22:02:01 +0300

    library rspec outline

---
+++ OOP_Library/spec/library_spec.rb
@@ -0,0 +1,7 @@
+require_relative '../library'
+
+describe Lib::Library do
+  it "should initialize without arguments"
+  it "should initialize with argument - path to data file"
+  it "should load data file"
+end
