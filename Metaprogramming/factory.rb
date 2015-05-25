9ff44d90257260388a4403f373ff23fa89f356e6
kprudnikov <kprudnikov@gmail.com>
2015-05-25 20:06:49 +0300

    factory complete

--- Metaprogramming/factory.rb
+++ Metaprogramming/factory.rb
@@ -10,25 +10,18 @@ class Factory
 
       define_method :initialize do |*elements|
         elements.each.with_index do |el, index|
-          instance_variable_set("@#{args[index]}".to_sym, el)          
+          instance_variable_set("@#{args[index]}".to_sym, el)
         end
       end
 
       def [](arg)
         if (arg.is_a? String) || (arg.is_a? Symbol)
-          instance_variable_get arg
+          instance_variable_get "@#{arg}"
         elsif arg.is_a? Fixnum
           instance_variables[arg]
         end
       end
-      
+
     end
   end
 end
-
-Pers = Factory.new(:name)
-john = Pers.new("Smith")
-puts john.inspect
-puts john.name
-puts john[1]
-puts john[:name]
