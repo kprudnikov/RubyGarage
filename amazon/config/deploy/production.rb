a52db4a6fd96fedfa0661154f3236e79726c6fe3
kprudnikov <kprudnikov@gmail.com>
2015-08-16 23:33:23 +0300

    deploy

--- amazon/config/deploy/production.rb
+++ amazon/config/deploy/production.rb
@@ -59,3 +59,8 @@
 #     auth_methods: %w(publickey password)
 #     # password: 'please use keys'
 #   }
+
+set :stage, :production
+
+# Replace 127.0.0.1 with your server's IP address!
+server '127.0.0.1', user: 'deploy', roles: %w{web app}
